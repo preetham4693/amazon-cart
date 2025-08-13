c3da655bbdb497bdc3d83c56b5cfd5da6078a444
preetham4693 <npreetham993@gmail.com>
2025-08-13 10:28:03 +0530

    add products.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,28 +1,4 @@
-const products = [{
-  Image : "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars:4.5,
-    count:87
-  },
-  priceCents:1090 //in cents 1$ = 100 cents
-},{
-  Image : "images/products/intermediate-composite-basketball.jpg",
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars:4,
-    count:127
-  },
-  priceCents:2095
-},{
-  Image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars:4.5,
-    count:56
-  },
-  priceCents:799
-}]
+
 
 //*10 at rating because the rating images are defined as() 40,50,45 )not (4,5,4.5)
 //.tofixed() is to get 2 decimal points here
@@ -32,7 +8,7 @@ products.forEach((product)=>{
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
-          src="${product.Image}">
+          src="${product.image}">
       </div>
 
       <div class="product-name limit-text-to-2-lines">
