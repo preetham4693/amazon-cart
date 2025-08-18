387062bb5ca21400700c51554a082971c95d7736
preetham4693 <npreetham993@gmail.com>
2025-08-18 18:43:29 +0530

    removes item from page

--- script/checkout.js
+++ script/checkout.js
@@ -11,12 +11,12 @@ cart.forEach((item)=>{
   if(product.id===productId){
     matchingProducts=product
   }
- })
- console.log(matchingProducts);
+ });
 
 cartSummaryHtml+=
 
-`<div class="cart-item-container">
+`<div class="cart-item-container 
+js-cart-item-container-${matchingProducts.id}">
 <div class="delivery-date">
   Delivery date: Tuesday, June 21
 </div>
@@ -103,7 +103,9 @@ document.querySelectorAll('.js-delete-quantity')
     link.addEventListener('click',()=>{
       const productId= link.dataset.productId;
       removeFromCart(productId)
-      console.log(cart)
+     
+      const container= document.querySelector(`.js-cart-item-container-${productId}`)
+      container.remove()
 
     })
   })
